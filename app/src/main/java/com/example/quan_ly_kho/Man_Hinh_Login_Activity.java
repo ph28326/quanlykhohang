6438d837378cab136ce50e03ad5056a2e0c977cd
tungddph28817 <tungddph28817@fpt.edu.vn>
2023-03-15 13:48:32 +0700

    tùng _ sửa lại màn hình login

---
+++ app/src/main/java/com/example/quan_ly_kho/Man_Hinh_Login_Activity.java
@@ -0,0 +1,14 @@
+package com.example.quan_ly_kho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Man_Hinh_Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_login);
+    }
+}
